e2093c790e9538d822db360e12c0189668eed442
Rwebb1981 <76513305+Rwebb1981@users.noreply.github.com>
2021-01-06 06:29:17 +0000

    Add files via upload

---
+++ Plot code for ggplot2.R
@@ -0,0 +1,56 @@
+#Code to load the 'Diamonds' dataset which comes with ggplot2 and plot an empty ggplot chart (The aes argument stands for aesthetics)
+library(ggplot2)
+ggplot(diamonds)  # if only the dataset is known.
+ggplot(diamonds, aes(x=carat))  # if only X-axis is known. The Y-axis can be specified in respective geoms.
+ggplot(diamonds, aes(x=carat, y=price))  # if both X and Y axes are fixed for all layers.
+ggplot(diamonds, aes(x=carat, color=cut))  # Each category of the 'cut' variable will now have a distinct  color, once a geom is added.
+
+#Code to ensure that the colour is fixed and not change dependent on the variable (this is achieved by entering the colouroutside of the aes)
+ggplot(diamonds, aes(x=carat), color="steelblue")
+
+#Code to add the geometric points to the chart. 
+ggplot(diamonds, aes(x=carat, y=price, color=cut)) + geom_point() + geom_smooth() # Adding scatterplot geom (layer1) and smoothing geom (layer2).
+ggplot(diamonds) + geom_point(aes(x=carat, y=price, color=cut)) + geom_smooth(aes(x=carat, y=price, color=cut)) # Same as above but specifying the aesthetics inside the geoms.
+
+#Code to integrate all the 'cuts' into a single line (this is achieved by removing the 'color' aes from the 'geom_smooth' layer)
+library(ggplot2)
+ggplot(diamonds) + geom_point(aes(x=carat, y=price, color=cut)) + geom_smooth(aes(x=carat, y=price)) # Remove color from geom_smooth
+ggplot(diamonds, aes(x=carat, y=price)) + geom_point(aes(color=cut)) + geom_smooth()  # same but simpler
+
+
+
+
+#Code to load the 'ggplot' library and the 'midwest' data and initiate 'ggplot'
+# Setup
+options(scipen=999)  # turn off scientific notation like 1e+06
+library(ggplot2)
+data("midwest", package = "ggplot2")  # load the data
+# midwest <- read.csv("http://goo.gl/G1K41K") # alt source 
+
+# Init Ggplot
+ggplot(midwest, aes(x=area, y=poptotal))  # area and poptotal are columns in 'midwest'
+
+#Code to create a scatterplot using the 'geom_point' function
+library(ggplot2)
+ggplot(midwest, aes(x=area, y=poptotal)) + geom_point()
+
+#Code to add a smoothing layer and 
+library(ggplot2)
+g <- ggplot(midwest, aes(x=area, y=poptotal)) + geom_point() + geom_smooth(method="lm")  # set se=FALSE to turnoff confidence bands
+plot(g)
+
+#Code to adjust the x and y axis (useful when we want to see what happens when outliers are removed)
+library(ggplot2)
+g <- ggplot(midwest, aes(x=area, y=poptotal)) + geom_point() + geom_smooth(method="lm")  # set se=FALSE to turnoff confidence bands
+
+# Delete the points outside the limits
+g + xlim(c(0, 0.1)) + ylim(c(0, 1000000))   # deletes points
+# g + xlim(0, 0.1) + ylim(0, 1000000)   # deletes points
+
+#Code to 'zoom in' without deleting any data points (using 'coord_cartesian' function)
+# Zoom in without deleting the points outside the limits. 
+# As a result, the line of best fit is the same as the original plot.
+g1 <- g + coord_cartesian(xlim=c(0,0.1), ylim=c(0, 1000000))  # zooms in
+plot(g1)
+
+#Code to 
